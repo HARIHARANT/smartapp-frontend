762a416a8f0a391626bb41dda0408b1eced5b7bb
Hariharan Thangavel <30747099+HARIHARANT@users.noreply.github.com>
2022-08-23 07:50:08 +0530

    Update Checkout.js

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -26,6 +26,15 @@ import FormHelperText from '@mui/material/FormHelperText';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams,
+    useNavigate
+  } from "react-router-dom";
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -41,7 +50,8 @@ const ExpandMore = styled((props) => {
 export default function Checkout() {
     const [expanded, setExpanded] = useState(false);
     const [cartItems, setCartItems] = useState([]);
-
+    const navigate = useNavigate();
+    let totalAmt = 0;
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
@@ -49,87 +59,76 @@ export default function Checkout() {
     async function fetcCartItems() {
         try {
             let request = {
-                userId:localStorage.getItem("userId")
+                userId: localStorage.getItem("userId").toString()
             };
 
-            let response = await HttpService("get/cart", 'GET', request, 'Live');
+            let response = await HttpService("get/cart", 'POST', request, 'Live');
             console.log(response);
-            if (response.status == 200) {
+            if (response.status == 200 && response.data.success) {
                 console.log("Inside success");
-                console.log(response.data.records);
-                setCartItems(response.data.records);
+                console.log(response.data.data);
+                setCartItems(response.data.data);
+            } else {
+                setCartItems([]);
             }
         } catch (e) {
             console.log(e);
         }
     }
-    
-  const [name, setName] = useState('Composed TextField');
-  const handleChange = (event) => {
-    setName(event.target.value);
-  };
+
+    const [name, setName] = useState('Composed TextField');
+    const handleChange = (event) => {
+        setName(event.target.value);
+    };
 
 
     console.log("cartItems final");
     console.log(cartItems);
     useEffect(() => {
-        fetcCartItems();
+        if(localStorage.getItem("_token")){
+            fetcCartItems();
+        }else{
+            navigate('/login');
+        }
+        
     }, []);
 
     return (
         <>
-            <Header />            
+            <Header />                        
             <h1>Checkout</h1>
             <Box sx={{ flexGrow: 1 }}>
                 <Grid container spacing={2} sx={{ padding: "20px" }}>
                     <Grid item md={8} xs={12} sx={{ alignContent: 'center' }}>
-                        <Card>
-                            <CardHeader
-                                avatar={
-                                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                                        R
-                                    </Avatar>
-                                }                               
-                                title="Shrimp and Chorizo Paella"
-                                subheader="September 14, 2016"
-                            />
-                            <CardContent>
-                                <Typography variant="body1" color="text.primary" sx={{ fontWeight: "bold" }}>ssfs</Typography>
-                                <Typography variant="body2" color="text.secondary" sx={{
-                                    whiteSpace: "nowrap",
-                                    overflow: "hidden",
-                                    textOverflow: "ellipsis"
-                                }}>
-                                    test
-                                </Typography>
-                            </CardContent>
-                        </Card><br />
-                        <Card>
-                            <CardHeader
-                                avatar={
-                                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                                        R
-                                    </Avatar>
-                                }                               
-                                title="Shrimp and Chorizo Paella"
-                                subheader="September 14, 2016"
-                            />
-                            <CardContent>
-                                <Typography variant="body1" color="text.primary" sx={{ fontWeight: "bold" }}>ssfs</Typography>
-                                <Typography variant="body2" color="text.secondary" sx={{
-                                    whiteSpace: "nowrap",
-                                    overflow: "hidden",
-                                    textOverflow: "ellipsis"
-                                }}>
-                                    test
-                                </Typography>
-                            </CardContent>
-                        </Card><br />
+                        {
+                            
+                            cartItems.map((data,index) => {
+                                let avatarChar = data.productsData[0].title[0];
+                                totalAmt+=Number(data.productsData[0].price);
+                                return (
+                                    <>
+                                        <Card key={index}>
+                                            <CardHeader
+                                                avatar={
+                                                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                                                        {avatarChar}
+                                                    </Avatar>
+                                                }
+                                                title={                                                
+                                                    data.productsData[0].title                                                   
+                                                }
+                                                subheader={`Rs.${data.productsData[0].price}`}
+                                            />                                            
+                                        </Card><br />
+                                    </>
+                                );
+                            })
+                        }                        
                     </Grid>
                     <Grid item md={4} xs={12} sx={{ alignContent: 'center' }}>
-                        <Card>                            
+                        <Card>
                             <CardContent>
-                                <Typography variant="body1" color="text.primary" sx={{ fontWeight: "bold" }}>Total Amount: </Typography>
+                                <Typography variant="body1" color="text.primary" sx={{ fontWeight: "bold" }}>Total Amount: Rs {totalAmt.toFixed(2)}</Typography>
                                 <Typography variant="body2" color="text.secondary" sx={{
                                     whiteSpace: "nowrap",
                                     overflow: "hidden",
@@ -139,7 +138,7 @@ export default function Checkout() {
                                 </Typography>
                             </CardContent>
                             <CardActions disableSpacing>
-                            <Button variant="outlined">PayNow</Button>
+                                <Button variant="outlined">PayNow</Button>
                             </CardActions>
                         </Card>
                     </Grid>
@@ -159,15 +158,15 @@ export default function Checkout() {
                                         value={name}
                                         onChange={handleChange}
                                         aria-describedby="component-helper-text"
-                                    />                                    
+                                    />
                                     <FormHelperText id="component-helper-text">
                                         Some important helper text
                                     </FormHelperText>
                                 </FormControl>
                             </CardContent>
-                        </Card><br />                        
-                    </Grid>                    
-                </Grid>              
+                        </Card><br />
+                    </Grid>
+                </Grid>
             </Box>
         </>
     );
